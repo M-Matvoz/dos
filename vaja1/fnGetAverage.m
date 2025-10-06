a1305e62289488567085e9da09fca678d4cd2ed2
MartinMatvoz <matvoz.martin@gmail.com>
2025-10-06 13:17:24 +0200

    Added error checking

--- vaja1/fnGetAverage.m
+++ vaja1/fnGetAverage.m
@@ -1,4 +1,14 @@
 function avg = fnGetAverage(x)
 %% Funkcija izracuna povprecje elementov vektorja x
-avg = sum(x)/length(x); % povprecje je vsota deljena s stevilom elementov
+if isempty(x)
+    error('Vektor x je prazen!');
+end
+if ~isvector(x)
+    error('Vhod x ni vektor!');
+end
+if isscalar(x)
+    avg = x; % ce je vektor enoelementen, je povprecje enako elementu
+    return;
+else
+    avg = sum(x)/length(x); % povprecje je vsota deljena s stevilom elementov
 end
