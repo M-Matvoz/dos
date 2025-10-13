de31a411c6373c8ba7b1d99cddcb18839ac0909a
Martin Matvoz <matvoz.martin@gmail.com>
2025-10-13 12:04:56 +0200

    Fixed Correlation function

--- vaja1/fnCorrelationFunction.m
+++ vaja1/fnCorrelationFunction.m
@@ -1,19 +1,23 @@
-function z = fnCorrelationFunction(x, y, m)
+function z = fnCorrelationFunction(x, y)
 %% Funkcija izracuna korelacijo med signaloma x in y
 %% x - prvi vhodni signal
 %% y - drugi vhodni signal
-%% m - zamik (lag) za izracun korelacije
 %% z - izhodna korelacija med signaloma x in y
 N = length(x); % dolzina signala
 if length(y) ~= N
     error('Signala morata biti enake dolzine');
 end
-if abs(m) >= N
-    error('Zamik ne sme biti vecji ali enak dolzini signala');
+results = zeros(1, N);
+for i = 0:N-1
+    sum = 0;
+    for n = 1:N
+        if (n + i) <= N
+            sum = sum + x(n) * y(n + i);
+        else
+            sum = sum + x(n) * y(n + i - N);
+        end
+    end
+    results(i + 1) = sum / N; % izracun korelacije
 end
-sum = 0;
-for n = 1:N-1
-    sum = sum + x(n) * y(n + m);
-end
-z = sum / N; % izracun korelacije
+z = results; % izracun korelacije
 end
