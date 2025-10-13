bb0ef6432831d834bfba4656b8815a10b006d3d2
Martin Matvoz <matvoz.martin@gmail.com>
2025-10-13 12:05:22 +0200

    Fixed generateSinus function

--- vaja0/fnGenerateSinus.m
+++ vaja0/fnGenerateSinus.m
@@ -8,10 +8,10 @@ n = 0:N-1; % diskretni casovni vektor, n = 1:N
 % Za drugacen korak je sintaksa n = 0:korak:N-1;
 y = sin(2*pi*n/N); % generiranje sinusnega signala
 
-figure; % odpre novo okno za risanje
-% plot(n, y); % risanje signala - zvezno
-stem(n, y); % risanje signala z markerji - ni zvezno
-xlabel('$n$','interpreter','latex', 'FontSize', 14); % oznaka x-osi
-ylabel('$y[n]$','interpreter','latex', 'FontSize', 14); % oznaka y-osi
-grid on; % vklop mreze
+% figure; % odpre novo okno za risanje
+% % plot(n, y); % risanje signala - zvezno
+% stem(n, y); % risanje signala z markerji - ni zvezno
+% xlabel('$n$','interpreter','latex', 'FontSize', 14); % oznaka x-osi
+% ylabel('$y[n]$','interpreter','latex', 'FontSize', 14); % oznaka y-osi
+% grid on; % vklop mreze
 end
